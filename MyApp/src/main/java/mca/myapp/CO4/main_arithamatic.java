f5f075f008b6d7ca946ea60c223e83bb2113e239
achsahgeorge007 <achsahmg18@gmail.com>
2022-07-11 13:19:42 +0530

    sdaksf

---
+++ MyApp/src/main/java/mca/myapp/CO4/main_arithamatic.java
@@ -0,0 +1,13 @@
+package mca.myapp.CO4;
+import mca.myapp.CO4.arithamatic_package.*;
+
+class main_arithamatic {
+    public static void main(String []args){
+        arithamatic_package testObj = new arithamatic_package();
+        System.out.println(testObj.add(1,2));
+        System.out.println(testObj.substract(1,2));
+        System.out.println(testObj.multiple(1,2));
+        System.out.println(testObj.divide(5,2));
+        System.out.println(testObj.remainder(11,2));
+    }
+}
